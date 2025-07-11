87e1f2f61d2c3eb04eda723d20556877742e4434
Antoine Noël <noelantoine974@outlook.fr>
2025-07-11 16:34:16 +0400

    mise ajour route reservation

--- API/routes/ReservationRoutes.js
+++ API/routes/ReservationRoutes.js
@@ -4,6 +4,5 @@ const { verifyToken } = require('../middleware/AuthClient');
 const reservationController = require('../controllers/ReservationController');
 
 router.post('/create', verifyToken, reservationController.creerReservation);
-router.get('/mes-reservations', verifyToken, reservationController.getMesReservations);
 
 module.exports = router;
